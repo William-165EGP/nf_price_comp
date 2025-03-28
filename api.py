dc36d7dee3a54992d1f23e5b45848452dabb4254
William-165EGP <1234567weewee457@gmail.com>
2025-03-29 03:17:24 +0800

    get rid of the country that doesn't exist

--- api.py
+++ api.py
@@ -3,12 +3,15 @@ import json
 
 country_codes=[]
 
+country_not_exist = ['KS', 'C1', 'Y1', 'T1', 'Y2', 'T2', 'M3', 'D1', 'M1', 'E1', 'D2', 'M2', 'JG', 'D3', 'Y3']
+
 for i in range(0, 300, 100):
     response = requests.get(
         f'https://public.opendatasoft.com/api/explore/v2.1/catalog/datasets/countries-codes/records?select=iso2_code&limit=100&offset={i}')
     raw_json = json.loads(response.text)
     for one_country in raw_json['results']:
-        country_codes.append(one_country['iso2_code'])
+        if one_country['iso2_code'] not in country_not_exist:
+            country_codes.append(one_country['iso2_code'])
 #    print(i)
 
 with open('api.csv', 'w') as f:
