529a6f60ae8f64d0359f0e560c52bfe7968e924f
William-165EGP <1234567weewee457@gmail.com>
2025-03-29 01:05:00 +0800

    only premium

---
+++ api.py
@@ -0,0 +1,25 @@
+import requests
+import json
+
+country_codes=[]
+
+for i in range(0, 300, 100):
+    response = requests.get(
+        f'https://public.opendatasoft.com/api/explore/v2.1/catalog/datasets/countries-codes/records?select=iso2_code&limit=100&offset={i}')
+    raw_json = json.loads(response.text)
+    for one_country in raw_json['results']:
+        country_codes.append(one_country['iso2_code'])
+    print(i)
+
+with open('api.csv', 'w') as f:
+    for code in country_codes:
+        f.write(f"{code}\n")
+
+print(country_codes)
+print(len(country_codes))
+
+
+
+
+
+
---
+++ static.py
@@ -0,0 +1,91 @@
+import time
+import requests
+from bs4 import BeautifulSoup
+import re
+import json
+import os
+
+def get_current_price(country_codes):
+    price_list = {}
+    for country_code in country_codes:
+        url = 'https://help.netflix.com/en/node/24926/' + country_code.lower()
+        print(url)
+        response = requests.get(url)
+        time.sleep(0.5)
+        while response.url!=url:
+            time.sleep(0.5)
+            print('reget')
+            print(response.url)
+            response=requests.get(url)
+        #print(country_code)
+        if response.status_code == 200:
+            soup = BeautifulSoup(response.text, "html.parser")
+            price_plans = {}
+            try:
+                pricing_sections = soup.find_all(string=lambda text: ": " in text and "month" in text)
+                pricing_sections=pricing_sections[len(pricing_sections)-1]
+                print(len(pricing_sections))
+                print(pricing_sections)
+                raw_price = pricing_sections
+                print(raw_price)
+                # print(raw_text)
+                # print(country_code, plan_name, raw_price)
+                # price like 9699,99 in AR or 229.99 in TR can also be applied
+                price = re.sub(r"[^\d.,]", "", raw_price)
+            except IndexError:  # For country like China, Russia aren't available.
+                price = 0
+            if price != 0:
+                price_plans['Premium'] = price
+
+
+            #print(price_plans)
+
+            price_list[country_code] = price_plans
+
+        else:
+            print("Request failed.")
+    return price_list
+
+def get_country_codes():
+    with open('api.csv', 'r', encoding='utf-8') as f:
+        country_codes = [line.strip() for line in f.readlines()]
+    return country_codes
+
+def comparison(prev_price_list, cur_price_list):
+    msg = ''
+    if prev_price_list == cur_price_list:
+       msg += 'Previous price list is equal to current price list'
+    else:
+        msg += 'List of different price:'
+        for key, value in prev_price_list.items():
+            if value != cur_price_list.get(key):
+                msg += '\nPrice of {} is different.'.format(key) + ' Fuck you Netflix!!'
+
+    return msg
+
+def sendMSG(token, msg):
+
+    debugMode = True
+    if debugMode:
+        print(msg)
+    else:
+        url = "https://api.telegram.org/bot{}/sendMessage".format(token)
+        payload = {
+            "chat_id": -1002141784679,
+            "text": msg,
+        }
+        res = requests.post(url, data=payload)
+
+if __name__ == '__main__':
+    country_codes = get_country_codes()
+    price_list = get_current_price(country_codes)
+
+    with open('static.json', 'r', encoding='utf-8') as f:
+        prev_price_list = json.load(f)
+
+    msg = comparison(prev_price_list, price_list)
+    token = os.getenv("MY_TGBOT_TOKEN")
+    sendMSG(token, msg)
+
+    with open('static.json', 'w', encoding='utf-8') as f:
+        json.dump(price_list, f, ensure_ascii=False, indent=4)
