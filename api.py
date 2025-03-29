0d18fa7332611479d4e86de5ffae4f15e2f97461
William-165EGP <1234567weewee457@gmail.com>
2025-03-29 17:00:21 +0800

    remove all print

--- api.py
+++ api.py
@@ -14,13 +14,13 @@ raw_countries = response.text.split('\n')[1:]
 for raw_one_country in raw_countries:
     one_country = raw_one_country.rsplit(',', 1)
     one_country[0] = one_country[0].strip('"')
-    print(one_country[0])
+#    print(one_country[0])
     if one_country[0] != '' and one_country[0] not in country_not_exist:
         one_country[0], one_country[1] = one_country[1], one_country[0]
         country_codes.append(one_country)
 
 country_codes.sort(key=lambda x: x[0])
-print(country_codes)
+#print(country_codes)
 
 with open("api.csv", "w", newline="") as f:
     for row in country_codes:
